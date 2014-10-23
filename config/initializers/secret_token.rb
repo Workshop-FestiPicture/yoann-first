15639d5ecf65cfc93b6b8d6f7e44011d87beb741
Yoann Perez <yoann.perez@hotmail.fr>
2014-10-23 18:42:26 +0200

    mon tout premier commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoannFirst::Application.config.secret_token = '9ae86a4cfafc0b40841e545f785f6393cb5b20745ca7ea5b6962d3bd1120cae7008e7345eb5cd967ccf29f2e3b81f2a9510b584eb63fae0ed5b1ee4670aa24a7'
